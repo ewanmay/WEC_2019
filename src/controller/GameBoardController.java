28bc8da9c4e30746564966b93afdcd991c5ddfb3
Dylan-Gordon <dylan.gordon.10@gmail.com>
2019-01-18 10:36:53 -0700

    Added icon to basin buttons

--- src/controller/GameBoardController.java
+++ src/controller/GameBoardController.java
@@ -1,7 +1,11 @@
 package controller;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -110,7 +114,14 @@ public class GameBoardController {
             //set text (number of adjacent basins)
             int buttonDisplay = board.boardAt(row, col);
             if(buttonDisplay == -1) {
-            		button.setIcon(new ImageIcon("/res/square.png"));  
+            		ImageIcon imageIcon = new ImageIcon("res/square.png");
+            		Image image = imageIcon.getImage();
+            		Image newImage = image.getScaledInstance(25, 25,  java.awt.Image.SCALE_SMOOTH);
+            		imageIcon = new ImageIcon(newImage);
+            		button.setIcon(imageIcon);  
+
+            		gui.repaint();
+            		
             		JOptionPane.showMessageDialog(null, "Basin Hit!", "Lion Inc.", JOptionPane.ERROR_MESSAGE);
             		reset();
             }
