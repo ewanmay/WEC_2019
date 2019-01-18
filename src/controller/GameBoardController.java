76a23115965914d0d9a733f79a40d007d07be102
Ross Bartlett <rossjbartlett@gmail.com>
2019-01-18 13:42:58 -0600

    added import Font

--- src/controller/GameBoardController.java
+++ src/controller/GameBoardController.java
@@ -4,6 +4,8 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
