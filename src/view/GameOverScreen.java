c6e211b7b9d9eae852beccff8712ed1b369fa8ad
Dylan-Gordon <dylan.gordon.10@gmail.com>
2019-01-18 08:49:19 -0700

    Added JavaDocs to GameOverScreen

--- src/view/GameOverScreen.java
+++ src/view/GameOverScreen.java
@@ -1,8 +1,18 @@
 package view;
 
 import javax.swing.JOptionPane;
+/**
+ * GameOverScreen.java allows the selection of starting a game over again after completion or failure of the current game.
+ *
+ */
 
 public class GameOverScreen {
+	
+	/**
+	 * Takes in a message that tells the user if they won or lost the game. 
+	 * @param message if the user has  won or lost
+	 * @return true if user would like to play again, false if user does not
+	 */
 	
 	public Boolean gameOver(String message) {
 		
