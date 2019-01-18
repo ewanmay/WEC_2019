42657cc45604b768547ef281c675a4de142c056b
Dylan-Gordon <dylan.gordon.10@gmail.com>
2019-01-18 08:11:28 -0700

    Changed formatting of startScreen

--- src/view/StartScreen.java
+++ src/view/StartScreen.java
@@ -4,14 +4,13 @@ import javax.swing.JOptionPane;
 
 public class StartScreen {
 	
-	StartScreen(){
-		
-	}
+	StartScreen(){}
 	
 	public int SelectBoardSize() {
 		String[] inputButtons = {"Small", "Medium", "Large"};
 		
-		int result = JOptionPane.showOptionDialog(null, "Select Size of Board", "Select size of board", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, inputButtons, null);
+		int result = JOptionPane.showOptionDialog(null, "Select Size of Board", "Select size of board", 
+				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, inputButtons, null);
 		
 		if(result == JOptionPane.YES_OPTION) 
 		{
@@ -30,5 +29,4 @@ public class StartScreen {
 			return -1;
 		}
 	}
-
 }
